629ca1c91a4dc1aa4bfd6808dc90103dd05d1ad0
Dorian Marié <dorian@dorianmarie.com>
2024-04-03 05:50:17 +0200

    add tests

---
+++ spec/to_struct_spec.rb
@@ -0,0 +1,41 @@
+require "spec_helper"
+
+HASH = {
+  first_name: "Dorian",
+  last_name: "Marié",
+  events: [
+    { name: "Party!" }
+  ]
+}
+
+STRUCT = HASH.to_struct
+DEEP_STRUCT = HASH.to_deep_struct
+
+RSpec.describe "to_struct" do
+  describe "#to_struct" do
+    it "works" do
+      expect(STRUCT.first_name).to eq("Dorian")
+      expect(STRUCT.last_name).to eq("Marié")
+    end
+
+    it "raises an eerror on missing value" do
+      expect { STRUCT.birthday }.to raise_error(NoMethodError)
+    end
+
+    it "doesn't deep structify" do
+      expect { STRUCT.events.first.name }.to raise_error(NoMethodError)
+    end
+  end
+
+  describe "@to_deep_struct" do
+    it "works" do
+      expect(DEEP_STRUCT.first_name).to eq("Dorian")
+      expect(DEEP_STRUCT.last_name).to eq("Marié")
+      expect(DEEP_STRUCT.events.first.name).to eq("Party!")
+    end
+
+    it "raises an eerror on missing value" do
+      expect { DEEP_STRUCT.birthday }.to raise_error(NoMethodError)
+    end
+  end
+end
